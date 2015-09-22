71910109b9e1766e20de9b929461af4f39bd4b03
sabz <sbareeshk1991@gmail.com>
2015-09-22 18:32:17 +0530

    the changes made for updating the pagination to the views

--- bucket.py
+++ bucket.py
@@ -3,9 +3,10 @@ import urllib2
 import urllib
 import base64
 import json
-from flask import Flask
 
+from flask import Flask
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
@@ -13,9 +14,12 @@ def hello_world():
     result = api.get_commits('rawdatatech', 'proman', 3)
     return json.dumps(result)
 
+
 class Bitbucket():
+
     def __init__(self, username, password, proxy=None):
-        encodedstring = base64.encodestring("%s:%s" % (username, password))[:-1]
+        encodedstring = base64.encodestring(
+            "%s:%s" % (username, password))[:-1]
         self._auth = "Basic %s" % encodedstring
         self._opener = self._create_opener(proxy)
 
@@ -32,24 +36,27 @@ class Bitbucket():
     def get_issues(self, username, repository):
         query_url = 'https://bitbucket.org/api/2.0/repositories/'
         return self._get_result(query_url)
-      
+
     def get_commits(self, username, repository, page=1):
         print username
         query_params = urllib.urlencode({'page': page})
         print query_params
-        query_url = 'https://bitbucket.org/api/2.0/repositories/%s/%s/commits?%s' % (username, repository, query_params)
+        query_url = 'https://bitbucket.org/api/2.0/repositories/%s/%s/commits?%s' % (
+            username, repository, query_params)
         print query_url
         result = self._get_result(query_url)
         return result
 
     def get_comments(self, username, repository, revision):
-        query_url = 'https://bitbucket.org/api/2.0/repositories/%s/%s/commit/%s/comments' % (username, repository, revision)
+        query_url = 'https://bitbucket.org/api/2.0/repositories/%s/%s/commit/%s/comments' % (
+            username, repository, revision)
         result = self._get_result(query_url)
         return result
 
     def _get_result(self, query_url):
         try:
-            req = urllib2.Request(query_url, None, {"Authorization": self._auth })
+            req = urllib2.Request(
+                query_url, None, {"Authorization": self._auth})
             handler = self._opener.open(req)
         except urllib2.HTTPError, e:
             print e.headers
