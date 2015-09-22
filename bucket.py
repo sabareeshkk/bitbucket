510da1d949b7447b4d627c0f314dfcf156d5a335
sabz <sbareeshk1991@gmail.com>
2015-09-22 18:24:22 +0530

    the pagination is made to the apis

--- bucket.py
+++ bucket.py
@@ -1,8 +1,8 @@
 import cookielib
 import urllib2
+import urllib
 import base64
 import json
-import pprint
 from flask import Flask
 
 app = Flask(__name__)
@@ -10,12 +10,10 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     api = Bitbucket('sabareeshk', 'valiyakkunnu')
-    result = api.get_commits('rawdatatech', 'proman', '41fa551e4d2315fe3d107595fbba026b9b181ac3')
+    result = api.get_commits('rawdatatech', 'proman', 3)
     return json.dumps(result)
 
 class Bitbucket():
-    api_url = 'http://bitbucket.org/api/2.0/'
-
     def __init__(self, username, password, proxy=None):
         encodedstring = base64.encodestring("%s:%s" % (username, password))[:-1]
         self._auth = "Basic %s" % encodedstring
@@ -34,13 +32,21 @@ class Bitbucket():
     def get_issues(self, username, repository):
         query_url = 'https://bitbucket.org/api/2.0/repositories/'
         return self._get_result(query_url)
-        
-    def get_commits(self, username, repository, revision):
-        query_url = 'https://bitbucket.org/api/2.0/repositories/%s/%s/commits/%s' % (username, repository, revision)
+      
+    def get_commits(self, username, repository, page=1):
+        print username
+        query_params = urllib.urlencode({'page': page})
+        print query_params
+        query_url = 'https://bitbucket.org/api/2.0/repositories/%s/%s/commits?%s' % (username, repository, query_params)
         print query_url
         result = self._get_result(query_url)
         return result
-    
+
+    def get_comments(self, username, repository, revision):
+        query_url = 'https://bitbucket.org/api/2.0/repositories/%s/%s/commit/%s/comments' % (username, repository, revision)
+        result = self._get_result(query_url)
+        return result
+
     def _get_result(self, query_url):
         try:
             req = urllib2.Request(query_url, None, {"Authorization": self._auth })
